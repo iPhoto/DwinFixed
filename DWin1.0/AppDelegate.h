b450de1a4b0eb2a4accc2c26b979547fbd71a14b
Zangqilong <zangqilong@gmail.com>
2013-09-12 16:05:17 +0800

    Initial Commit

---
+++ DWin1.0/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DWin1.0
+//
+//  Created by zang qilong on 13-9-12.
+//  Copyright (c) 2013年 zang qilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DWin1.0/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DWin1.0
+//
+//  Created by zang qilong on 13-9-12.
+//  Copyright (c) 2013年 zang qilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
